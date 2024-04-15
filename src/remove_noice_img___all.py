b45e8b34369ee8be4c915e9e51ce9383a1a6e64a
AbdoHerO <abdo.sbeta.12@gmail.com>
2024-04-15 13:03:12 +0100

    change pc sophatel 15-04-2024 13:00

--- src/remove_noice_img___all.py
+++ src/remove_noice_img___all.py
@@ -5,10 +5,10 @@ import numpy as np
 # *--------------------------------------------------------* Step 1 *--------------------------------------------------------*
 
 #Reading the image
-for i in range( 8  ):
+for i in range(8):
     
-        img_name = 'model0' + str(i + 1)
-        img = cv2.imread('../data/models/' + img_name + '.jpeg')
+        img_name = 'BLS_' + str(i + 1)
+        img = cv2.imread('../data/models/' + img_name + '.jpg')
 
         # Convert the image to grayscale
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -31,32 +31,32 @@ for i in range( 8  ):
         sharpened___ = cv2.addWeighted(img, 5.5, gaussian_blur, -4.5, 0)
 
         # Save the sharpened image
-        cv2.imwrite('../output_models/binary_' + img_name + '.jpeg', binary)
-        cv2.imwrite('../output_models/sharpened_' + img_name + '.jpeg', sharpened___)
+        cv2.imwrite('../output_models/binary_' + img_name + '.jpg', binary)
+        cv2.imwrite('../output_models/sharpened_' + img_name + '.jpg', sharpened___)
 
-        cv2.imwrite('../data/binary_' + img_name + '.jpeg', binary)
+        cv2.imwrite('../data/binary_' + img_name + '.jpg', binary)
 
         # *--------------------------------------------------------* Step 2 *--------------------------------------------------------*
 
-        img_to_transformer_step_2 = cv2.imread('../data/binary_' + img_name + '.jpeg')
+        img_to_transformer_step_2 = cv2.imread('../data/binary_' + img_name + '.jpg')
         rows, cols = img_to_transformer_step_2.shape[:2]
 
         #Bilateral filtring (Reduction of noise + Preserving of edges)
         output_bil = cv2.bilateralFilter(img_to_transformer_step_2, 5,6,6)
-        cv2.imwrite('../output_models/bilateral_' + img_name + '.jpeg', output_bil)
+        cv2.imwrite('../output_models/bilateral_' + img_name + '.jpg', output_bil)
 
 
 
         # *--------------------------------------------------------* Step 3 - Adaptive *--------------------------------------------------------*
 
-        img_name = 'model0' + str(i + 1)
-        img = cv2.imread('../data/models/' + img_name + '.jpeg')
+        img_name = 'BLS_' + str(i + 1)
+        img = cv2.imread('../data/models/' + img_name + '.jpg')
         img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
         _, result = cv2.threshold(img, 20, 255, cv2.THRESH_BINARY)
         adaptive_result = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 105, 5)
 
-        cv2.imwrite('../output_models/adaptive_' + img_name + '.jpeg', adaptive_result)
+        cv2.imwrite('../output_models/adaptive_' + img_name + '.jpg', adaptive_result)
         # *--------------------------------------------------------* Show all Windows *--------------------------------------------------------*
 
         cv2.waitKey(0)
--- src/tess_ocr__worked____git.py
+++ src/tess_ocr__worked____git.py
@@ -7,79 +7,94 @@ from reportlab.pdfgen import canvas
 # Set the path to the Tesseract executable
 tess.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
-# Open the image
-img_name = 'binary_model06'
-image = Image.open('../output_models/'+img_name+'.jpeg')
-# image = Image.open('../data/new_bl_rimini/'+img_name+'.jpg')
-# image = Image.open('../output_results/sharpened_image_cpd01_croped.jpg')
+#Reading the images Processed
+for i in range(1):
 
-# Extract text from the image
-text = image_to_string(image)
+    # Open the image
+    # img_name = 'BLS_' + str(i + 1) + '_Processed'
+    # image = Image.open('../output_models/' + img_name + '.jpg')
+    # img_name = 'BLS_' + str(i + 1)
+    img_name = 'BLS_1_croped'
+    image = Image.open('../data/models/' + img_name + '.jpg')
 
-# Split the text by line breaks
-text_lines = text.split('\n')
+    # Extract text from the image
+    text = image_to_string(image)
 
-# Create an ImageDraw object
-draw = ImageDraw.Draw(image)
+    # Split the text by line breaks
+    text_lines = text.split('\n')
+
+    # Create an ImageDraw object
+    draw = ImageDraw.Draw(image)
 
 
-# Create a PDF document
-pdf_path = '../output_results_tesseract_testing/'+img_name+'______.pdf'
-c = canvas.Canvas(pdf_path, pagesize=letter)
+    # Create a PDF document
+    pdf_path = '../output_results_tesseract_testing/'+img_name+'______.pdf'
+    c = canvas.Canvas(pdf_path, pagesize=letter)
 
-# Define font size and line height
-font_size = 12
-line_height = 1.5 * font_size
+    # Define font size and line height
+    font_size = 12
+    line_height = 1.5 * font_size
 
-# Iterate over each line to draw bounding boxes and text, and add text to PDF
-y_position = 700  # Starting y-position for text in PDF
+    # Iterate over each line to draw bounding boxes and text, and add text to PDF
+    y_position = 700  # Starting y-position for text in PDF
 
-# Print and write each line to a file with line breaks
-with open('../output_results_tesseract_testing/'+img_name+'_text.txt', 'w') as file:
+    # Track the current page
+    current_page = 0
+
+    # Print and write each line to a file with line breaks
+    with open('../output_results_tesseract_testing/'+img_name+'_text.txt', 'w') as file:
+        for line in text_lines:
+            print(line)
+            file.write(line + '\n')
+
+            # Add the text to the PDF document
+            c.setFont("Helvetica", font_size)
+            c.drawString(50, y_position, line )
+            y_position -= line_height
+
+    # Parse each line and draw bounding boxes
     for line in text_lines:
-        print(line)
-        file.write(line + '\n')
+        # Skip empty lines
+        if not line.strip():
+            continue
 
-        # Add the text to the PDF document
-        c.setFont("Helvetica", font_size)
-        c.drawString(50, y_position, line )
+        if y_position <= 0:
+            # Add a new page
+            c.showPage()
+            current_page += 1
+            y_position = 700
+
+        # Split line into character, left, top, right, bottom
+        parts = line.split()
+        try:
+            character = parts[0]
+            left, top, right, bottom = map(int, parts[1:5])
+        except ValueError:
+            # Skip lines that don't contain valid bounding box information
+            continue
+
+        # Draw bounding box and text
+        draw.rectangle([(left, top), (right, bottom)], outline='red')
+        draw.text((left, top), character, fill='red')
+
         y_position -= line_height
 
-# Parse each line and draw bounding boxes
-for line in text_lines:
-    # Skip empty lines
-    if not line.strip():
-        continue
-
-    # Split line into character, left, top, right, bottom
-    parts = line.split()
-    try:
-        character = parts[0]
-        left, top, right, bottom = map(int, parts[1:5])
-    except ValueError:
-        # Skip lines that don't contain valid bounding box information
-        continue
-
-    # Draw bounding box and text
-    draw.rectangle([(left, top), (right, bottom)], outline='red')
-    draw.text((left, top), character, fill='red')
 
 
+    # Save the image with bounding boxes and text
+    image_with_boxes_path = '../output_results_tesseract_testing/'+img_name+'.jpg'
+    image.save(image_with_boxes_path)
 
-# Save the image with bounding boxes and text
-image_with_boxes_path = '../output_results_tesseract_testing/'+img_name+'.jpg'
-image.save(image_with_boxes_path)
+    # Save the PDF document
+    c.save()
 
-# Save the PDF document
-c.save()
+    # Display the image
+    # image.show()
 
-# Display the image
-# image.show()
-
-# Print and write the recognized text to a file
-text_path = '../output_results_tesseract_testing/'+img_name+'_text.txt'
-# print("Bounding boxes and text drawn on the image. Image saved as:", image_with_boxes_path)
-print("Recognized text saved as:", text_path)
+    # Print and write the recognized text to a file
+    text_path = '../output_results_tesseract_testing/'+img_name+'_text.txt'
+    # print("Bounding boxes and text drawn on the image. Image saved as:", image_with_boxes_path)
+    print("Recognized text saved as:", text_path)
 
 
 
